c24b1845e85be8cc7172aaa13e4b9c018099afe9
Robert Collins <beau@collins.pub>
2018-10-08 18:36:04 -0700

    configurations

--- webpack.config.js
+++ webpack.config.js
@@ -2,37 +2,37 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { resolve } = require('path');
 
 module.exports = {
-  mode: 'production',
-  devtool: 'source-map',
-  context: resolve(__dirname, 'src'),
-  entry: './index.js',
-  output: {
-    filename: '[name].js',
-    path: resolve(__dirname, 'dist')
-  },
-	resolve: {
-    extensions: [ '.js', '.css', '.scss' ],
-    modules: [ 'src', 'node_modules' ],
+	mode: 'production',
+	devtool: 'source-map',
+	context: resolve(__dirname, 'src'),
+	entry: './app.jsx',
+	output: {
+		filename: '[name].js',
+		path: resolve(__dirname, 'dist')
 	},
-  module: {
-    rules: [
-      { test: /\.js$/, exclude: /(node_modules)/, use: {
-        loader: 'babel-loader', options: { presets: [ '@babel/preset-env' ] }
-      } },
+	resolve: {
+		extensions: [ '.js', '.css', '.scss', '.jsx' ],
+		modules: [ 'src', 'node_modules' ],
+	},
+	module: {
+		rules: [
+			{ test: /\.jsx?$/, exclude: /(node_modules)/, use: {
+				loader: 'babel-loader', options: { presets: [ '@babel/preset-env' ] }
+			} },
 			{ test: /\.s?css$/, use: [
 				{ loader: 'style-loader' },
 				{ loader: 'css-loader' },
 				{ loader: 'sass-loader' }
 			] }
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin( {
-      title: 'Proprietary Noise',
-      hash: true,
-      meta: {
-        viewport: 'width=device-width, initial-scale=1',
-      },
-    } )
-  ]
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin( {
+			title: 'Proprietary Noise',
+			hash: true,
+			meta: {
+				viewport: 'width=device-width, initial-scale=1',
+			},
+		} )
+	]
 };
